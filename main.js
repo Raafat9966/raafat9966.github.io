6425e89791a274807d1ac2a3249435b78bcafad1
Raafat <slipknot9966@gmail.com>
2020-07-30 18:44:40 +0200

    Adding the images

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const menuBtn = document.querySelector(".menu-btn");
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".gallery img");
+const original = document.querySelector(".full-img");
+const caption = document.querySelector(".caption");
+
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
 	menuBtn.classList.toggle("open");
@@ -10,4 +15,24 @@ menuBtn.addEventListener("click", () => {
 	//     menuBtn.classList.remove("open");
 	//     menuOpen = false;
 	// }
+});
+
+// photo section
+
+previews.forEach((preview) => {
+	preview.addEventListener("click", () => {
+		modal.classList.add("open");
+		original.classList.add("open");
+		const originalSrc = preview.getAttribute("data-original");
+		original.src = `./full/${originalSrc}`;
+		const altText = preview.alt;
+		caption.textContent = altText;
+	});
+});
+
+modal.addEventListener("click", (e) => {
+	if (e.target.classList.contains("modal")) {
+		modal.classList.remove("open");
+		original.classList.remove("open");
+	}
 });
